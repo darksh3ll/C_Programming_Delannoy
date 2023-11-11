a8e75a3680dc78924a6d61cdcb74722236b344d1
darksh3ll <darksh3ll.js@gmail.com>
2023-11-11 12:35:35 +0100

    wip

--- Chapter_02_C_Type/type.c
+++ Chapter_02_C_Type/type.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
-#include <stdbool.h>
 
-
-int main() {
-    int i = 5;
-    int k;
-    k = i++;
-    printf("%d\n",i);
-    printf("%d",k);
+int main() {         
+    unsigned char i = 0;
 }
