663d9fc1dcdcaced102608d24a2c50d897e18114
darksh3ll <darksh3ll.js@gmail.com>
2023-11-25 19:56:18 +0100

    wip

---
+++ Chapter_03_opérateurs_expressions/Exercices/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int n = 10, p = 5, q = 10, r = 1;
+    n += p += r++;
+    printf("n = %d\n", n);
+    printf("p = %d\n", p);
+    // printf("q = %d\n", q);
+    printf("r = %d\n", r);
+    return 0;
+}
