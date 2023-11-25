83922fa05af3eabdd69f5d5129d84c5c86d8cad5
darksh3ll <darksh3ll.js@gmail.com>
2023-11-25 20:14:57 +0100

    wip

--- Chapter_03_opérateurs_expressions/Exercices/exercices_4/main.c
+++ Chapter_03_opérateurs_expressions/Exercices/exercices_4/main.c
@@ -3,12 +3,13 @@ int main()
 {
     int n = 10, p = 5, q = 10, r;
     r = n == (p = q);
-    // r = 10 == ( 5 = 10)  
     printf("A : n = %d p = %d q = %d r = %d\n", n, p, q, r);
-    //Resultat: n = 10, p = 10, q = 10, r = 1
     n = p = q = 5;
-    // n = 5, p = 5, q = 5
     n += p += q;
-    // n = 15 p = 10 q = 5
-    printf("A : n = %d p = %d q = %d r = %d", n, p, q, r);
+    printf("b : n = %d p = %d q = %d r = %d", n, p, q, r);
+    q = n < p ? n++ : p++ ;
+    printf("C : n = %d p = %d q = %d r = %d", n, p, q, r);
+    q = n > p ? n++ : p++ ;
+    printf("C : n = %d p = %d q = %d r = %d", n, p, q, r);
+
 }
--- Chapter_03_opérateurs_expressions/Exercices/test.c
+++ Chapter_03_opérateurs_expressions/Exercices/test.c
@@ -2,10 +2,12 @@
 int main()
 {
     int n = 10, p = 5, q = 10, r = 1;
-    n += p += r++;
+    n += p += ++r;	
     printf("n = %d\n", n);
     printf("p = %d\n", p);
     // printf("q = %d\n", q);
     printf("r = %d\n", r);
+    return 0;
+
     return 0;
 }
