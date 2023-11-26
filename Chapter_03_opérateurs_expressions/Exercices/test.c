2cba03f54a06657dd6705acaaf58551c10dc5ba7
DARKSH3LL <darksh3ll.js@gmail.com>
2023-11-26 20:23:12 +0100

    wip

--- Chapter_03_opérateurs_expressions/Exercices/test.c
+++ Chapter_03_opérateurs_expressions/Exercices/test.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h> // Nécessaire pour la fonction abs()
+
 int main()
 {
-    int n = 10, p = 5, q = 10, r = 1;
-    n += p += ++r;	
-    printf("n = %d\n", n);
-    printf("p = %d\n", p);
-    // printf("q = %d\n", q);
-    printf("r = %d\n", r);
-    return 0;
-
-    return 0;
+    int c = 1;
+    int counter = 0;
 }
