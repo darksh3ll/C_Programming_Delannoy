849813856ffd452fa8fc94e078c745e6d7a98931
darksh3ll <darksh3ll.js@gmail.com>
2023-11-28 20:48:33 +0100

    wip

--- Chapter_02_C_Type/type.c
+++ Chapter_02_C_Type/type.c
@@ -2,11 +2,8 @@
 
 int main()
 {
-    int a, b, c, d;
-    a = b = c = d = 5;
-    int i = 0;
-    // int j = i++;
-    int k = i + 1;
-    printf("la variable i vaut %d \n",i);
-    printf("%d", k);
+    char c1 = 10;
+    printf("%d",c1+1);
+
+
 }
