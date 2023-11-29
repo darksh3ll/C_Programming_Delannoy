3660cea55473fec1716cd198830aaff64d9fb562
darksh3ll <darksh3ll.js@gmail.com>
2023-11-29 18:31:08 +0100

    wip

--- Chapter_03_opérateurs_expressions/Exercices/test.c
+++ Chapter_03_opérateurs_expressions/Exercices/test.c
@@ -3,15 +3,10 @@
 #include <stdio.h>
 
 int main() {
-<<<<<<< HEAD
-    const char* message = "Hello, world!\n";
-    syscall(SYS_write, STDOUT_FILENO, message, 13);
-=======
     int a, b;
     printf("Entrez deux nombres: ");
     scanf("%d", &a);
     scanf("%d", &b);
     printf("Vous avez entré %d et %d\n", a, b);
->>>>>>> master
     return 0;
 }
