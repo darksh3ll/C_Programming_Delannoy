a5a0959868e88e2636b013e79caf5a0adfb23712
darksh3ll <darksh3ll.js@gmail.com>
2023-12-03 17:54:10 +0100

    wip

--- Chapter_03_opérateurs_expressions/Exercices/test.c
+++ Chapter_03_opérateurs_expressions/Exercices/test.c
@@ -4,15 +4,6 @@
 
 int main()
 {
-<<<<<<< HEAD
     int age = 18;
     printf("%3d",++age);
-=======
-    int array[5] = {1, 2, 3, 4, 5};
-    for (int i = 0; i < 5; i++)
-    {
-        printf("%d",array[i]);
-    }
-    
->>>>>>> master
 }
