d531a40b683f71dd9ac5d9325c0f5cc4c0dffad7
darksh3ll <darksh3ll.js@gmail.com>
2023-12-03 12:32:03 +0100

    wip

--- Chapter_03_opérateurs_expressions/Exercices/test.c
+++ Chapter_03_opérateurs_expressions/Exercices/test.c
@@ -9,16 +9,10 @@ int count(int arr[]){
 };
 int main()
 {
-<<<<<<< HEAD
     int array[5] = {1, 2, 3, 4, 5};
     for (int i = 0; i < 5; i++)
     {
         printf("%d",array[i]);
     }
     
-=======
-    int array[] = {1, 2, 3, 4, 2, 5, 2, 10};
-    int y = count(array);
-    return 0;
->>>>>>> 97b67c5a92905139739682ed44e2b6a9db167796
 }
