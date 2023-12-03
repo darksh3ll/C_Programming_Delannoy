3f940b9e94e4336c78a33494e81f73a9f5d3b858
darksh3ll <darksh3ll.js@gmail.com>
2023-12-03 17:53:30 +0100

    wip

--- Chapter_03_opérateurs_expressions/Exercices/test.c
+++ Chapter_03_opérateurs_expressions/Exercices/test.c
@@ -2,14 +2,8 @@
 #include <sys/syscall.h>
 #include <stdio.h>
 
-int count(int arr[]){
-    int count = sizeof(arr)/sizeof(arr[0]);
-    return count;
-
-};
 int main()
 {
-    int array[] = {1, 2, 3, 4, 2, 5, 2, 10};
-    int y = count(array);
-    return 0;
+    int age = 18;
+    printf("%3d",++age);
 }
