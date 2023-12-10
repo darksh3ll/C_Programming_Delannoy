3ef1d105c8207da072ca91c41f9911b4aa6cb509
darksh3ll <darksh3ll.js@gmail.com>
2023-12-10 12:29:54 +0100

    wip

--- Chapter_04/main.c
+++ Chapter_04/main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
 int main(){
-    int space = 5;
     int a = 40;
+    int precision = 2;
+    int space = 0;
     float b = 1.20;
-    printf ("%3.*d\n", space, a) ;
-
-    printf("%3d\n",a);
-    printf("%10d\n",a);
+    float c = 5.12;
+    printf("%3d\n",a);//   40
+    printf("%10d\n",a);//          40
     printf("%f\n",b); //1,200000
-    printf("%.2f",b); // 1,20
+    printf("%.2f\n",b); // 1,20
+    printf("%f\n",c);
+    printf("%.*f\n",precision,c);
+    printf("%*d",space,a);
+
 }
