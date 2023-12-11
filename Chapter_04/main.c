4d517ba4f032bbd0113e8d854e79aa2701029357
darksh3ll <darksh3ll.js@gmail.com>
2023-12-11 21:01:15 +0100

    wip

--- Chapter_04/main.c
+++ Chapter_04/main.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
 
-<<<<<<< HEAD
 int main() {
     int a;
-    char c;a 
+    char c;
     printf("Entrez deux nombres séparés par un espace : ");
     scanf("%d %c", &a, &c);
     printf("Les nombres entrés sont : %d et %c\n", a, c);
     return 0;
-=======
-int main(){
-    int n;
-    printf("Entre une valeur :");
-    scanf("%d",&n);
-    printf("Voici la racine carré de %d %d \n",n,n*n);
-
->>>>>>> 9dad4a9c1e9acacf3ae76ac1bce32198098a7ec9
 }
