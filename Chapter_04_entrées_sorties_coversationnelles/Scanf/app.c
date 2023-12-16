eb684daee7c222958afa5b15d8ed5b449fa2e1cc
DARKSH3LL <darksh3ll.js@gmail.com>
2023-12-16 11:38:29 +0100

    wip

---
+++ Chapter_04_entrées_sorties_coversationnelles/Scanf/app.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+    char a;
+    printf("entrez un chiffre merci : ");
+    scanf("%c",&a);
+    printf("vous avez taper le chiffre %c\n",a);
+    return 0;
+
+}
