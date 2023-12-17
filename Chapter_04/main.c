a88fdb10302a789fa7867617262d65dbbaae6f95
DARKSH3LL <darksh3ll.js@gmail.com>
2023-12-17 12:10:32 +0100

    wip

--- Chapter_04/main.c
+++ Chapter_04/main.c
@@ -2,9 +2,12 @@
 
 int main()
 {
- int n,p;
- char c;
- printf("Entre 2 nombres : ");
- scanf("%d%c",&n,&c);
- printf("%d %c",n,c);
+//  int n,p;
+//  char c;
+//  printf("Entre 2 nombres : ");
+//  scanf("%d%c",&n,&c);
+//  printf("%d %c",n,c);
+    char c;
+    scanf("%c",&c);
+    printf("%c",c); 
 }
