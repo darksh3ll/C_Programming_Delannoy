163c0e70c847a78532b5f3dd3e2d3da2608286d2
darksh3ll <darksh3ll.js@gmail.com>
2023-12-20 19:51:25 +0100

    wip

--- Chapter_04_entrées_sorties_coversationnelles/Scanf/app.c
+++ Chapter_04_entrées_sorties_coversationnelles/Scanf/app.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+int ma_fonction()
+{   
+    return 2;
+}
+
+
 int main()
 {
-    char a;
-    printf("entrez un chiffre merci : ");
-    scanf("%c",&a);
-    printf("vous avez taper le chiffre %c\n",a);
+    int a = 12;
+    int b = 10;
+    char c = '1';
+    int toto = ma_fonction();
+    printf("%d",toto++);
+    // printf("entrez deux chiffre et un charactére : ");
+    // int compte = scanf("%d %d %c",&a, &b, &c);
+    // printf("%d",compte);
+    // printf("a = %d b = %d c = %c" ,a,b,c);
     return 0;
 }
