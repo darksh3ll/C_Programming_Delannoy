5014115b44e76af3f77627b488f925db72865867
darksh3ll <darksh3ll.js@gmail.com>
2023-12-21 16:02:42 +0100

    wip

--- Chapter_04_entrées_sorties_coversationnelles/Scanf/app.c
+++ Chapter_04_entrées_sorties_coversationnelles/Scanf/app.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-<<<<<<< HEAD
 int main()
 {
     int n;
@@ -9,13 +8,4 @@ int main()
         scanf("%d", &n);
         printf("voici son carré : %d\n", n * n);
     } while (n);
-=======
-
-int main()
-{
-    int n = 1;
-    printf("%d\n",n++);
-    printf("%d",n);
-    return 0;
->>>>>>> 1a6478ff60f17b0fbd1e709f1af2ffa3881b98f1
 }
