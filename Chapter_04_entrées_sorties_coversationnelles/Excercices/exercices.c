8057558e1df7b22de421b683fdc9e18eba7f8056
darksh3ll <darksh3ll.js@gmail.com>
2023-12-21 12:28:02 +0100

    wip

---
+++ Chapter_04_entrées_sorties_coversationnelles/Excercices/exercices.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 543;
+    int p = 5;
+    float x = 34.5678;
+    printf ("A : %d %f\n", n, x) ; //A : 543 34.5678
+    printf ("B : %4d %10f\n", n, x) ;
+    return 0;
+}
--- Chapter_04_entrées_sorties_coversationnelles/Scanf/app.c
+++ Chapter_04_entrées_sorties_coversationnelles/Scanf/app.c
@@ -1,20 +1,9 @@
 #include <stdio.h>
-int ma_fonction()
-{   
-    return 2;
-}
-
 
 int main()
 {
-    int a = 12;
-    int b = 10;
-    char c = '1';
-    int toto = ma_fonction();
-    printf("%d",toto++);
-    // printf("entrez deux chiffre et un charactére : ");
-    // int compte = scanf("%d %d %c",&a, &b, &c);
-    // printf("%d",compte);
-    // printf("a = %d b = %d c = %c" ,a,b,c);
+    int n = 1;
+    printf("%d\n",n++);
+    printf("%d",n);
     return 0;
 }
