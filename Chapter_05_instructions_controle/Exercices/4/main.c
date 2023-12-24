cfb9183107a86b961a8f81d1d247f94b040b7302
DARKSH3LL <darksh3ll.js@gmail.com>
2023-12-24 18:56:01 +0100

    wip

--- Chapter_05_instructions_controle/Exercices/4/main.c
+++ Chapter_05_instructions_controle/Exercices/4/main.c
@@ -2,15 +2,17 @@
 
 int main()
 {
-    int input, i;
- 
+    int input, i, j;
+
     printf("Entrez le nombres de lignes: ");
     scanf("%d", &input);
     for (i = 1; i <= input; i++)
     {
-        printf("%c\n", '*');
-       
+        for (j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
     }
-
     return 0;
 }
---
+++ Chapter_05_instructions_controle/Exercices/5/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define COUNT 20;
+int main()
+{
+    int i, j;
+    for (i = 1; i <= COUNT i++)
+
+    {
+        for (j = 1; j <= 10; j++)
+        {
+            printf("%4d", j * i);
+        }
+        printf("\n");
+    };
+
+    return 0;
+}
