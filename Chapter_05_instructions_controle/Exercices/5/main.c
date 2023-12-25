b0324d0c4ff03c4e643b0710cc41ba6d4191e8d3
darksh3ll <darksh3ll.js@gmail.com>
2023-12-25 12:04:23 +0100

    exercices boucles

--- Chapter_05_instructions_controle/Exercices/5/main.c
+++ Chapter_05_instructions_controle/Exercices/5/main.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
-#define COUNT 20;
+#define COUNT 10;
 int main()
 {
     int i, j;
+    printf("%9c",'I');
+    for (i = 1; i <= 10; i++)
+    {
+        
+        printf("%4d", i);
+    }
+    printf("\n");
+
+    for (i = 0; i < 50; i++)
+    {
+        printf("%c", '-');
+    }
+
+    printf("\n");
+
     for (i = 1; i <= COUNT i++)
 
     {
+        printf("%4d %4c", i, 'I');
+
         for (j = 1; j <= 10; j++)
         {
             printf("%4d", j * i);
