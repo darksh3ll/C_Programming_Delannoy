f2635d981ceb540688a3511e71ff5b30c501d06e
darksh3ll <darksh3ll.js@gmail.com>
2023-12-25 20:46:25 +0100

    wip

--- Chapter_05_instructions_controle/Exercices/6/main.c
+++ Chapter_05_instructions_controle/Exercices/6/main.c
@@ -2,12 +2,9 @@
 
 
 int main() {
-    float fexple(float, int, int); // Assurez-vous que la définition correspond quelque part dans votre code.
-    float x = 1.5;
-    float y;
-    int n = 3, p = 5;
-    y = fexple(x, n, p);
-    // Utilisez y ou faites quelque chose ici pour éviter les variables inutilisées.
-    printf("Result: %f\n", y);
+    int input;
+    printf("Entrez un nombre positif: ");
+    scanf("%d",&input);
+    printf("%d",input);
     return 0;
 }
