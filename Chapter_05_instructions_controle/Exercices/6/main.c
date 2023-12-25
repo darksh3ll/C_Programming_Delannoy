a53b844f3f1ac4ff363859d7b6fc5ae169ea4b77
darksh3ll <darksh3ll.js@gmail.com>
2023-12-25 12:53:43 +0100

    wip

--- Chapter_05_instructions_controle/Exercices/6/main.c
+++ Chapter_05_instructions_controle/Exercices/6/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-int main()
-{
 
+
+int main() {
+    float fexple(float, int, int); // Assurez-vous que la définition correspond quelque part dans votre code.
+    float x = 1.5;
+    float y;
+    int n = 3, p = 5;
+    y = fexple(x, n, p);
+    // Utilisez y ou faites quelque chose ici pour éviter les variables inutilisées.
+    printf("Result: %f\n", y);
     return 0;
 }
