cc0986fc1c706caf21190cd8851c8b93eb47edd9
darksh3ll <darksh3ll.js@gmail.com>
2023-12-27 15:14:04 +0100

    wip

--- Chapter_05_instructions_controle/Exercices/6/main.c
+++ Chapter_05_instructions_controle/Exercices/6/main.c
@@ -2,8 +2,8 @@
  * Title: Your Program Title
  * Author: darksh3ll <darksh3ll.js@gmail.com>
  * Created on: 2023-12
- * Description: Brief description of what the program does.
-
+ * Description: Programme qui verifie si un nombre est premier
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -12,6 +12,7 @@
  */
 
 #include <stdio.h>
+#include <math.h>
 
 // Function Declarations
 void exampleFunction();
@@ -20,8 +21,22 @@ void exampleFunction();
 int main()
 {
     // Variable Initialization
-    int i, x;
-    
+    int i = 2;
+    int input;
+    int input_racine_carre;
+
+    printf("Entrez un nombres: ");
+    scanf("%d", &input);
+    input_racine_carre = sqrt(input);
+
+    for (i; i <= input_racine_carre; i++)
+    {
+        if ((input % i) == 0)
+        {
+            printf("Le nombre %d est premier",input);
+            break;
+        }
+    }
 
     return 0;
 }
---
+++ Chapter_05_instructions_controle/Exercices/6/tempCodeRunnerFile.c
@@ -0,0 +1,5 @@
+  // if (input % i == 0)
+        // {
+        //     printf("%d",input % i == 0);
+        //     break;
+        // }
