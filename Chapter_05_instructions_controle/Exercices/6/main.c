fa711e427b0c60367bfb4a0a9a25c45f1e9c7bc0
darksh3ll <darksh3ll.js@gmail.com>
2023-12-28 09:56:29 +0100

    wip

--- Chapter_05_instructions_controle/Exercices/6/main.c
+++ Chapter_05_instructions_controle/Exercices/6/main.c
@@ -1,10 +1,34 @@
+/*
+ * Title: Your Program Title
+ * Author: darksh3ll <darksh3ll.js@gmail.com>
+ * Created on: 2023-12
+ * Description: Brief description of what the program does.
+
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ */
+
 #include <stdio.h>
+#include <math.h>
 
+// Function Declarations
+void exampleFunction();
 
-int main() {
-    int input;
-    printf("Entrez un nombre positif: ");
-    scanf("%d",&input);
-    printf("%d",input);
+// Main program
+int main()
+{
+    // Variable Initialization
+    double num = 29.0; // Remplacez par le nombre de votre choix
+    double racineCarree = sqrt(num);
+
+    printf("La racine carrée de %.2f est %.2f\n", num, racineCarree);
     return 0;
+}
+// Function Definitions
+void exampleFunction()
+{
+    // Function code
 }
