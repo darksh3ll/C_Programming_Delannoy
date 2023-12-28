ffac60b155e5b276e48c8fb37e3b0a0810d0eb7c
darksh3ll <darksh3ll.js@gmail.com>
2023-12-28 09:59:29 +0100

    wip

--- Chapter_05_instructions_controle/Exercices/6/main.c
+++ Chapter_05_instructions_controle/Exercices/6/main.c
@@ -2,13 +2,8 @@
  * Title: Your Program Title
  * Author: darksh3ll <darksh3ll.js@gmail.com>
  * Created on: 2023-12
-<<<<<<< HEAD
- * Description: Brief description of what the program does.
-
-=======
  * Description: Programme qui verifie si un nombre est premier
  *
->>>>>>> 42932d2a5816a59c6bd634a2778bcc6bab5213a9
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -26,12 +21,6 @@ void exampleFunction();
 int main()
 {
     // Variable Initialization
-<<<<<<< HEAD
-    double num = 29.0; // Remplacez par le nombre de votre choix
-    double racineCarree = sqrt(num);
-
-    printf("La racine carrée de %.2f est %.2f\n", num, racineCarree);
-=======
     int i = 2;
     int input;
     int input_racine_carre;
@@ -56,17 +45,11 @@ int main()
         printf("Le nombre %d est  premier", input);
     }
 
->>>>>>> 42932d2a5816a59c6bd634a2778bcc6bab5213a9
     return 0;
 }
 // Function Definitions
 void exampleFunction()
 {
-<<<<<<< HEAD
-    // Function code
-}
-=======
-    printf("hello");
+
 }
 
->>>>>>> 42932d2a5816a59c6bd634a2778bcc6bab5213a9
